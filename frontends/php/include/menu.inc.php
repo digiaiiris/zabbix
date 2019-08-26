58342e75b23b171413600567ed25471585ac8811
Jokinen Peter <peter.jokinen@digia.com>
2019-08-26 12:38:34 +0300

    IISCRUM-89 zabbix changes

--- frontends/php/include/menu.inc.php
+++ frontends/php/include/menu.inc.php
@@ -289,7 +289,15 @@ function zbx_construct_menu(&$main_menu, &$sub_menus, &$page, $action = null) {
 					'label' => _('Queue')
 				]
 			]
-		],
+      ],
+      'iiris' => [
+         'label' => _('Iiris extensions'),
+         'user_type' => USER_TYPE_SUPER_ADMIN,
+         'default_page_id' => 0,
+         'pages' => [
+            ['url' => 'testing.php','label' => _('Testing')]
+         ]
+      ],
 		'login' => [
 			'label' => _('Login'),
 			'user_type' => 0,
